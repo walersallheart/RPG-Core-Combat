6e73bf6e95dce3866c55407d369fd5e063b83435
finalmegaman NA <finalmegaman@gmail.com>
2020-01-21 19:21:17 -0800

    14 Implement Click to Move.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,17 +7,22 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
 
-    Ray lastRay;
-
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0)) {
-            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            MoveToCursor();
         }
+    }
 
-        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+    private void MoveToCursor(){
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
 
-        GetComponent<NavMeshAgent>().destination = target.position;
+        bool hasHit = Physics.Raycast(ray, out hit);
+
+        if (hasHit) {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
     }
 }
