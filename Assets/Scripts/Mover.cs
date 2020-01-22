9475e734d5a8bd0b87760e343ceb39f520381249
finalmegaman NA <finalmegaman@gmail.com>
2020-01-21 20:18:17 -0800

    18 Basic Movement Tweaks.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -10,7 +10,7 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) {
+        if (Input.GetMouseButton(0)) {
             MoveToCursor();
         }
 
