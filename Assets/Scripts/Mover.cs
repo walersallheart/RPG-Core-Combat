7f34ac310e4657a6040d6a70e13626d8237a401c
finalmegaman NA <finalmegaman@gmail.com>
2020-01-21 20:11:19 -0800

    17 Match Animation to Movement.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -13,6 +13,8 @@ public class Mover : MonoBehaviour
         if (Input.GetMouseButtonDown(0)) {
             MoveToCursor();
         }
+
+        UpdateAnimator();
     }
 
     private void MoveToCursor(){
@@ -24,5 +26,13 @@ public class Mover : MonoBehaviour
         if (hasHit) {
             GetComponent<NavMeshAgent>().destination = hit.point;
         }
+    }
+
+    private void UpdateAnimator(){
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("fowardSpeed",speed);
     }
 }
