d8177500258b070b23b9f3c2c45d8c122b9815f7
finalmegaman NA <finalmegaman@gmail.com>
2020-02-18 19:35:11 -0800

    29 Move Within Range.

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -1,12 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using RPG.Movement;
 
 namespace RPG.Combat {
     public class Fighter : MonoBehaviour
     {
-        public void Attack(CombatTarget target){
-            Debug.Log("Attack!");
+
+        [SerializeField] float weaponRange = 2f;
+
+        Transform target;
+        private void Update() {
+            bool isInRange = Vector3.Distance(transform.position, target.position) < weaponRange;
+
+            if (target != null && !isInRange) {
+                GetComponent<Mover>().MoveTo(target.position);
+            } else {
+                GetComponent<Mover>().Stop();
+            }
+        }
+        public void Attack(CombatTarget combatTarget){
+            target = combatTarget.transform;
         }
     }
 }
--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -8,6 +8,12 @@ namespace RPG.Movement {
     {
         [SerializeField] Transform target;
 
+        NavMeshAgent navMeshAgent;
+
+        private void Start() {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
         // Update is called once per frame
         void Update()
         {
@@ -15,11 +21,16 @@ namespace RPG.Movement {
         }
 
         public void MoveTo(Vector3 destination){
-            GetComponent<NavMeshAgent>().destination = destination;
+            navMeshAgent.destination = destination;
+            navMeshAgent.isStopped = false;
+        }
+
+        public void Stop(){
+            navMeshAgent.isStopped = true;
         }
 
         private void UpdateAnimator(){
-            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+            Vector3 velocity = navMeshAgent.velocity;
             Vector3 localVelocity = transform.InverseTransformDirection(velocity);
 
             float speed = localVelocity.z;
