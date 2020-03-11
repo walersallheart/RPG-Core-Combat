52c20a803abf93a4fabf1d1ef51b4a4b0a210231
finalmegaman NA <finalmegaman@gmail.com>
2020-03-10 19:22:47 -0700

    52 Visualizing Waypoints.

---
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control{
+    public class PatrolPath : MonoBehaviour
+    {
+        const float waypointGizmoRadius = .3f;
+
+        private void OnDrawGizmos() {
+            for (int i = 0; i < transform.childCount; i++){
+                Gizmos.DrawSphere(transform.GetChild(i).transform.position, waypointGizmoRadius);
+            }
+        }
+    }
+}
