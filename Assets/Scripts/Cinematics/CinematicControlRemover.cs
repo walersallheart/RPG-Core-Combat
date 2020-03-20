69041af4f3ec0d0b8839c7da741d12c424c57c25
finalmegaman NA <finalmegaman@gmail.com>
2020-03-19 19:26:13 -0700

    66 The Observer Pattern In C#.

---
+++ Assets/Scripts/Cinematics/CinematicControlRemover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics{
+    public class CinematicControlRemover : MonoBehaviour
+    {
+        private void Start() {
+            GetComponent<PlayableDirector>().played += DisableControl;
+            GetComponent<PlayableDirector>().stopped += EnableControl;
+        }
+
+        void DisableControl(PlayableDirector director){
+            Debug.Log("DisableControl()");
+        }
+
+        void EnableControl(PlayableDirector director){
+            Debug.Log("EnableControl()");
+        }
+    }
+}
