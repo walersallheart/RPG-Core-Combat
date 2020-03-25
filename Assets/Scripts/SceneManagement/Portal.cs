7049c27e027a43f51389f950aacef806b08fbdfb
finalmegaman NA <finalmegaman@gmail.com>
2020-03-24 19:36:14 -0700

    70 Scene Loading Portals.

---
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement {
+    public class Portal : MonoBehaviour{
+
+        [SerializeField] int sceneToLoad = -1;
+        private void OnTriggerEnter(Collider other) {
+            if (other.tag == "Player") {
+                SceneManager.LoadScene(sceneToLoad);
+            }
+        }
+    }
+}
