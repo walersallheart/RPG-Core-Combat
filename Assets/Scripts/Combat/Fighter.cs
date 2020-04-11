99afe92009adc3c89131258e8d8a899c1fbf6eb2
finalmegaman NA <finalmegaman@gmail.com>
2020-04-11 14:39:00 -0700

    109 Animator Override Controller.

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -13,6 +13,7 @@ namespace RPG.Combat {
         [SerializeField] float weaponDamage = 5f;
         [SerializeField] GameObject weaponPrefab = null;
         [SerializeField] Transform handTransform = null;
+        [SerializeField] AnimatorOverrideController weaponOverride = null;
 
         Health target;
         float timeSinceLastAttack = Mathf.Infinity;
@@ -37,6 +38,8 @@ namespace RPG.Combat {
 
         void SpawnWeapon(){
             Instantiate(weaponPrefab, handTransform);
+            Animator animator = GetComponent<Animator>();
+            animator.runtimeAnimatorController = weaponOverride;
         }
 
         private void AttackBehavior()
