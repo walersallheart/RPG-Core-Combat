a8988b64074333cd31a2fa2fbff24e624b4bcf81
finalmegaman NA <finalmegaman@gmail.com>
2020-04-21 19:06:43 -0700

    119 Improved Projectile Behavior.

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -7,20 +7,26 @@ namespace RPG.Combat{
     public class Projectile : MonoBehaviour
     {
         [SerializeField] float speed = 1f;
+        [SerializeField] bool isHoming = true;
         Health target;
         float damage = 0;
+
+        private void Start() {
+            transform.LookAt(GetAimLocation());
+        }
+
+        void Update(){
+            if (transform == null) return;
+
+            if (isHoming && !target.IsDead()) {
+                transform.LookAt(GetAimLocation());
+            }
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
 
         public void SetTarget(Health target, float damage) {
             this.target = target;
             this.damage = damage;
-        }
-
-        void Update()
-        {
-            if (transform == null) return;
-
-            transform.LookAt(GetAimLocation());
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
 
         private Vector3 GetAimLocation(){
@@ -33,6 +39,7 @@ namespace RPG.Combat{
 
         private void OnTriggerEnter(Collider other) {
             if (other.GetComponent<Health>() != target) return;
+            if (target.IsDead()) return;
             target.TakeDamage(damage);
             Destroy(gameObject);
         }
