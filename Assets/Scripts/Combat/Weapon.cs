ac00972f24710e5a0e2b25fdece3f6a9565746b7
finalmegaman NA <finalmegaman@gmail.com>
2020-04-23 19:38:54 -0700

    124 Resetting The Default Animator.

--- Assets/Scripts/Combat/Weapon.cs
+++ Assets/Scripts/Combat/Weapon.cs
@@ -24,8 +24,12 @@ namespace RPG.Combat{
                 weapon.name = weaponName;
             }
 
+            var overrideController = animator.runtimeAnimatorController as AnimatorOverrideController;
+
             if (animatorOverride != null) {
                 animator.runtimeAnimatorController = animatorOverride;
+            } else if (overrideController != null) {
+                animator.runtimeAnimatorController = overrideController.runtimeAnimatorController;
             }
         }
 
