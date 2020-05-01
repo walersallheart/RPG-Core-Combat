313d53f9ec9d0d276cec356265174c9c58cd1042
finalmegaman NA <finalmegaman@gmail.com>
2020-04-30 20:35:32 -0700

    138 Performant Lookups With Dictionaries.

--- Assets/Scripts/Stats/Progression.cs
+++ Assets/Scripts/Stats/Progression.cs
@@ -1,24 +1,39 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 namespace RPG.Stats{
     [CreateAssetMenu(fileName = "Progression", menuName = "Stats/New Progression", order = 0)]
     public class Progression : ScriptableObject {
         [SerializeField] ProgressionCharacterClass[] characterClasses = null;
 
+        Dictionary<CharacterClass, Dictionary<Stat, float []>> lookupTable = null;
+
         public float GetStat(Stat stat, CharacterClass characterClass, int level) {
-            foreach(ProgressionCharacterClass progressionClass in characterClasses) {
-                if (progressionClass.characterClass != characterClass) continue;
+            BuildLookup();
 
-                foreach(ProgressionStat progressionStat in progressionClass.stats){
-                    if (progressionStat.stat != stat) continue;
+            float[] levels = lookupTable[characterClass][stat];
 
-                    if (progressionStat.levels.Length < level) continue;
-
-                    return progressionStat.levels[level - 1];
-                }
+            if (levels.Length < level) {
+                return 0;
             }
 
-            return 0;
+            return levels[level-1];
+        }
+
+        private void BuildLookup(){
+            if (lookupTable != null) { return; }
+
+            lookupTable = new Dictionary<CharacterClass, Dictionary<Stat, float[]>>();
+
+            foreach(ProgressionCharacterClass progressionClass in characterClasses){
+                var statLookupTable = new Dictionary<Stat, float[]>();
+
+                foreach(ProgressionStat progressionStat in progressionClass.stats){
+                    statLookupTable[progressionStat.stat] = progressionStat.levels;
+                }
+
+                lookupTable[progressionClass.characterClass] = statLookupTable;
+            }
         }
 
         [System.Serializable]
