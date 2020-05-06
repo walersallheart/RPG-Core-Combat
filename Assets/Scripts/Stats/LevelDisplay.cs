9fa0e857cb61364da599f70ae75efed3c426df5a
finalmegaman NA <finalmegaman@gmail.com>
2020-05-05 19:48:49 -0700

    140 Displaying The Level.

---
+++ Assets/Scripts/Stats/LevelDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RPG.Stats{
+    public class LevelDisplay : MonoBehaviour
+    {
+        BaseStats stats;
+
+        private void Awake() {
+            stats = GameObject.FindWithTag("Player").GetComponent<BaseStats>();
+        }
+
+        private void Update() {
+            GetComponent<Text>().text = String.Format("{0:0}", stats.GetLevel());
+        }
+    }
+}
