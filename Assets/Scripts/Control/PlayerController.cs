00e1847efb66bab609803a22e913be7c3767d07a
finalmegaman NA <finalmegaman@gmail.com>
2020-05-19 19:41:52 -0700

    154 Configurable Cursors.

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -10,6 +10,21 @@ namespace RPG.Control {
     {
         Health health;
 
+        enum CursorType {
+            None,
+            Movement,
+            Combat
+        }
+
+        [System.Serializable]
+        struct CursorMapping {
+            public CursorType type;
+            public Texture2D texture;
+            public Vector2 hotspot;
+        }
+
+        [SerializeField] CursorMapping[] cursorMappings = null;
+
         private void Awake() {
             health = GetComponent<Health>();
         }
@@ -17,7 +32,8 @@ namespace RPG.Control {
             if (health.IsDead()) { return; }
             if (InteractWithCombat()) { return; }
             if (InteractWithMovement()) { return; }
-            Debug.Log("Nothing to do");
+
+            SetCursor(CursorType.None);
         }
 
         bool InteractWithCombat(){
@@ -36,10 +52,27 @@ namespace RPG.Control {
                 if (Input.GetMouseButton(0)) {
                     GetComponent<Fighter>().Attack(target.gameObject);
                 }
+
+                SetCursor(CursorType.Combat);
                 return true;
             }
 
             return false;
+        }
+
+        void SetCursor(CursorType type){
+            CursorMapping mapping = GetCursorMapping(type);
+            Cursor.SetCursor(mapping.texture, mapping.hotspot, CursorMode.Auto);
+        }
+
+        private CursorMapping GetCursorMapping(CursorType type) {
+            foreach(CursorMapping mapping in cursorMappings) {
+                if (mapping.type == type) {
+                    return mapping;
+                }
+            }
+
+            return cursorMappings[0];
         }
 
         bool InteractWithMovement(){
@@ -54,6 +87,7 @@ namespace RPG.Control {
                     GetComponent<Mover>().StartMoveAction(hit.point, 1f);
                 }
 
+                SetCursor(CursorType.Movement);
                 return true;
             }
 
