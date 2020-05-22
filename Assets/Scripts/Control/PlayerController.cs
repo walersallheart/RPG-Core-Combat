e86fb60dd32f12cae3cc938a3910f9fdfa4b886c
finalmegaman NA <finalmegaman@gmail.com>
2020-05-21 19:27:40 -0700

    158 Raycast Sorting.

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine.EventSystems;
 using RPG.Movement;
 using RPG.Combat;
 using RPG.Resources;
+using System;
 
 namespace RPG.Control {
     public class PlayerController : MonoBehaviour
@@ -45,7 +46,7 @@ namespace RPG.Control {
         }
 
         bool InteractWithComponent(){
-            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            RaycastHit[] hits = RaycastAllSorted();
 
             foreach (RaycastHit hit in hits){
                 IRaycastable[] raycastables = hit.transform.GetComponents<IRaycastable>();
@@ -59,6 +60,20 @@ namespace RPG.Control {
             }
 
             return false;
+        }
+        RaycastHit[] RaycastAllSorted() {
+
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+
+            float[] distances = new float[hits.Length];
+
+            for (int i = 0; i<hits.Length; i++) {
+                distances[i] = hits[i].distance;
+            }
+
+            Array.Sort(distances, hits);
+
+            return hits;
         }
 
         void SetCursor(CursorType type){
