dff0a11a3a71c189780eb5e3fae56acae14c126a
finalmegaman NA <finalmegaman@gmail.com>
2020-05-26 19:52:13 -0700

    Spawning Damage Text.

---
+++ Assets/Scripts/UI/DamageText/DamageTextSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.UI.DamageText{
+    public class DamageTextSpawner : MonoBehaviour
+    {
+        [SerializeField] DamageText damageTextPrefab = null;
+
+        void Start()
+        {
+            
+        }
+
+        public void Spawn(float damageAmount) {
+            DamageText instance = Instantiate<DamageText>(damageTextPrefab, transform);
+        }
+    }
+}
