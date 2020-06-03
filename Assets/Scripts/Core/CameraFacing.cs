9b3694ad8b25b543236135dfc009460b54f769fd
finalmegaman NA <finalmegaman@gmail.com>
2020-06-02 19:31:12 -0700

    Camera Facing LateUpdate Fix.

--- Assets/Scripts/Core/CameraFacing.cs
+++ Assets/Scripts/Core/CameraFacing.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 namespace RPG.Core {
     public class CameraFacing : MonoBehaviour
     {
-        void Update()
+        void LateUpdate()
         {
             transform.forward = Camera.main.transform.forward;
         }
