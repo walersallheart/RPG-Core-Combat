3d7f824fc1b2219389facd139a503dd5ebea16ae
finalmegaman NA <finalmegaman@gmail.com>
2020-06-02 19:10:56 -0700

    170 Fixing The Fader.

--- Assets/Scripts/SceneManagement/Fader.cs
+++ Assets/Scripts/SceneManagement/Fader.cs
@@ -6,6 +6,7 @@ namespace RPG.SceneManagement{
     public class Fader : MonoBehaviour
     {
         CanvasGroup canvasGroup;
+        Coroutine currentlyActiveFade = null;
 
         private void Awake() {
             canvasGroup = GetComponent<CanvasGroup>();
@@ -16,15 +17,26 @@ namespace RPG.SceneManagement{
         }
 
         public IEnumerator FadeOut(float time){
-            while (canvasGroup.alpha < 1) {
-                canvasGroup.alpha += Time.deltaTime / time;
-                yield return null;
-            }
+            return Fade(1, time);
         }
 
         public IEnumerator FadeIn(float time){
-            while (canvasGroup.alpha > 0) {
-                canvasGroup.alpha -= Time.deltaTime / time;
+            return Fade(0, time);
+        }
+
+        public IEnumerator Fade(float target, float time) {
+            if (currentlyActiveFade != null) {
+                StopCoroutine(currentlyActiveFade);
+            }
+
+            currentlyActiveFade = StartCoroutine(FadeRoutine(target, time));
+
+            yield return currentlyActiveFade;
+        }
+
+        private IEnumerator FadeRoutine(float target, float time) {
+            while (!Mathf.Approximately(canvasGroup.alpha, target)) {
+                canvasGroup.alpha = Mathf.MoveTowards(canvasGroup.alpha, target, Time.deltaTime / time);
                 yield return null;
             }
         }
