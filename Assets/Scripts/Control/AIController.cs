7fffcc1122ebd5ff7b095208687cd2c5f4fa81c5
finalmegaman NA <finalmegaman@gmail.com>
2020-06-11 18:45:27 -0700

    185 Enemy Mob Mechanics.

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -6,6 +6,7 @@ using RPG.Core;
 using RPG.Movement;
 using RPG.Attributes;
 using GameDevTV.Utils;
+using System;
 
 namespace RPG.Control {
     public class AIController : MonoBehaviour
@@ -18,6 +19,7 @@ namespace RPG.Control {
         [SerializeField] float waypointDwellTime = 3f;
         [Range(0,1)]
         [SerializeField] float patrolSpeedFraction = 0.2f;
+        [SerializeField] float shoutDistance = 5f;
 
         Fighter fighter;
         Health health;
@@ -108,6 +110,23 @@ namespace RPG.Control {
         void AttackBehavior(){
             timeSinceLastSawPlayer = 0;
             fighter.Attack(player);
+
+            AggrevateNearbyEnemies();
+        }
+
+        private void AggrevateNearbyEnemies()
+        {
+            RaycastHit[] hits = Physics.SphereCastAll(transform.position, shoutDistance, Vector3.up, 0);
+
+            foreach (RaycastHit hit in hits) {
+                AIController ai = hit.collider.GetComponent<AIController>();
+
+                if (ai == null) {
+                    continue;
+                }
+
+                ai.Aggravate();
+            }
         }
 
         bool IsAggravated() {
